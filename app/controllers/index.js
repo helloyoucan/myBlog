9423c766ffa3909f798aab6a8ba9653c50c99dee
helloyoucan <helloyoucan@163.com>
2017-07-10 19:01:11 +0800

    设置跨域请求

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -22,29 +22,30 @@ exports.index = function (req, res) {
 }
 //获取个人信息
 exports.getDetail = function (req, res) {
-    /*PersonalDetails.find({}, function (err, message) {
-     if (err) {
-     console.log(err);
-     } else {
-     console.log(message)
-     res.json(message)
-     }
-     });*/
-    var test = {
-        iconUrl: "img/head-icon.jpg",
-        name: "helloyoucan",
-        emial: ["helloyoucan@163.com", "helloyoucan520@gmail.com"],
-        github: {name: 'Github', url: 'https://github.com/helloyoucan', iconUrl: 'img/github.png'},
-        other: [{name: '简书', url: 'http://www.jianshu.com/u/9207c99d25fc', iconUrl: 'img/jianshu.jpg'}],
-    };
-    res.json(test)
-}
-exports.save = function (req, res) {
-    PersonalDetails.update({}, function (err, message) {
+    PersonalDetails.find({}, function (err, message) {
         if (err) {
             console.log(err);
         } else {
             console.log(message)
+            res.json(message)
+        }
+    });
+    /*var test = {
+     iconUrl: "img/head-icon.jpg",
+     name: "helloyoucan",
+     emial: ["helloyoucan@163.com", "helloyoucan520@gmail.com"],
+     github: {name: 'Github', url: 'https://github.com/helloyoucan', iconUrl: 'img/github.png'},
+     other: [{name: '简书', url: 'http://www.jianshu.com/u/9207c99d25fc', iconUrl: 'img/jianshu.jpg'}],
+     };*/
+    res.json(test)
+}
+exports.save = function (req, res) {
+    res.json({test:'ok'});
+    PersonalDetails.update({}, function (err, message) {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log(message);
         }
     })
 }
--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,11 @@
 var Index = require('../app/controllers/index');
 var Article = require('../app/controllers/article');
 var Bookmarks = require('../app/controllers/bookmarks');
+var cors = require('cors');
 module.exports = function (app) {
     app.get('/', Index.index);
     app.get('/getPersonalDetails', Index.getDetail);
+    app.post('/savePersonalDetails',cors(), Index.save);
     app.get('/article/:id', Article.article);
     app.get('/bookmarks', Bookmarks.bookmarks);
 }
